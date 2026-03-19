302ba831a2d6efb391541800cb4f5584516bd77e
Prem <premprakashjena04@gmail.com>
2026-03-20 03:52:24 +0530

    Build signup and login API using Django REST Framework

---
+++ README.md
@@ -0,0 +1,24 @@
+# Django Auth API Task
+
+This project contains basic Signup and Login APIs built using Django and Django REST Framework.
+
+## Features
+
+- User Signup API
+- User Login API
+- Django built-in User model
+- Secure password hashing using `create_user()`
+
+## Tech Stack
+
+- Python
+- Django
+- Django REST Framework
+- SQLite
+
+## Project Setup
+
+### 1. Clone the repository
+```bash
+git clone <your-repo-link>
+cd django-auth-task
---
+++ accounts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data.get('username'),
+            password=data.get('password')
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+
+        data['user'] = user
+        return data
